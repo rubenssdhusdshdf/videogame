cdcc8f5aa2a86c5c7b7804e26f577431b918a6bb
Ruben <rubenvegajusto@gmail.com>
2023-06-03 13:54:37 +0100

    displaying elements in the canvas

--- game.js
+++ game.js
@@ -1,9 +1,29 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 
-window.addEventListener("load", startGame); // helps to execute the function when the window loads
+window.addEventListener("load", startGame);
 
+// Maintain proportions in the canvas element
 function startGame() {
-  game.fillStyle = "purple";
-  game.font = "25px Verdana";
+  let canvasSize;
+
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
+
+  const elementsSize = canvasSize / 10;
+
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "end";
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis["X"], elementsSize, elementsSize * i);
+  } // We are entering the value X in the object emojis saved in maps.js, so it will give me the property
 }
