6aa604904f9302dbf052ce0648fcf354b6d457ce
Ruben <rubenvegajusto@gmail.com>
2023-06-05 20:49:37 +0100

    losing lives

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ const btnDown = document.querySelector("#down");
 let canvasSize;
 let elementsSize;
 let level = 0;
+let lives = 3;
 
 const playerPosition = {
   x: undefined,
@@ -104,7 +105,7 @@ function movePlayer() {
   });
 
   if (enemyCollision) {
-    console.log("Chocaste contra un enemigo :(");
+    levelFail();
   }
 
   game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
@@ -113,6 +114,18 @@ function movePlayer() {
 function levelWin() {
   console.log("Subiste de nivel");
   level++;
+  startGame();
+}
+
+function levelFail() {
+  console.log("You crashed with a bomb");
+  lives--;
+  if (lives <= 0) {
+    level = 0;
+  }
+
+  playerPosition.x = undefined;
+  playerPosition.y = undefined;
   startGame();
 }
 
